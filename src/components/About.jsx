8f032e4d643c290507397d02e259fd78ff7c3945
Henry Rennell <henryrennell@gmail.com>
2023-07-13 15:58:20 +1000

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,9 @@ export default function About() {
             <h2>About Me</h2>
             
             <p>I'm a Software Engineer based in Melbourne, with a background in Healthcare.</p>
-            <p>I have pursued an education in Computer Science since 2020 (studying BCS majoring in Cyber Security At Swinburne University of Technology).</p>
-            <p> I am looking to get my foot into Tech as a Full-Stack <span className="bold">Software Developer</span> after finishing the <span className="bold">General Assembly Software Engineering Immersive</span> course, I have a passion for education, continuous improvement and most importantly all things tech.</p><p>Whilst I am continuing to pursue my Bachelor of Computer Science, I intend to build a career as Software Engineer and let my further studies compliment my work</p>
+            <p>I have always had a distinct interest in Technology, from building my first computer at age 12, to learning some hacking fundamentals in my teenage years and finally settling on learning to program throughout my years as a hobby.</p>
+            <p> I am looking to get my foot into Tech as a Full-Stack <span className="bold">Software Developer</span> after finishing the <span className="bold">General Assembly Software Engineering Immersive</span> course, I have a passion for education, continuous improvement and most importantly all things tech.</p>
+            <p>Whilst my programming specialty lay in JavaScript technologies, I have excellent programming and computer science fundamentals which put me in good stead to quickly learn and adapt to new languages and frameworks. Being somewhat interested in hacking in my younger years, I have also gained some education on cyber security best practises. </p>
         </div>
     )
 }
